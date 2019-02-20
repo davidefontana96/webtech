f3c4946626a67a76c8431d56be2872c1c64b25b2
alessandrodimatteo97 <alessandrodimatteo1997>
2019-02-20 18:42:36 +0100

    minor release

--- database/seeds/ShoesTableSeeder.php
+++ database/seeds/ShoesTableSeeder.php
@@ -13,17 +13,17 @@ class ShoesTableSeeder extends Seeder
     public function run()
     {
       $faker = Faker::create();
-      foreach (range(1,50) as $index) {
+      foreach (range(1,10) as $index) {
         DB::table('shoes')->insert([
         'created_at'     => $faker->dateTimeBetween($startDate = '-30 days', $endDate = 'now', $timezone = null),
         'name'           => $faker->word,
         'details'        => $faker->text($maxNbChars = 200),
         'sex'            => 'F',
-        'id_category'    => $faker->numberBetween($min = 1, $max = 20),
-        'id_style'       => $faker->numberBetween($min = 1, $max = 20),
-        'id_brand'       => $faker->numberBetween($min = 1, $max = 20),
-        'id_promo'       => $faker->numberBetween($min = 1, $max = 20),
-        'id_news'       => $faker->numberBetween($min = 1, $max = 100),
+        'id_category'    => $faker->numberBetween($min = 10, $max = 20),
+        'id_style'       => $faker->numberBetween($min = 10, $max = 20),
+        'id_brand'       => $faker->numberBetween($min = 10, $max = 20),
+        'id_promo'       => $faker->numberBetween($min = 10, $max = 20),
+        'id_news'       => $faker->numberBetween($min = 10, $max = 100),
         ]);
       }
     }
